7739807249be051769233e2b0ba6996bd6072dd3
farhanhameed010 <farhanhameed010@gmail.com>
2024-12-30 12:23:02 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,12 @@
 # File: terraform/main.tf
 provider "azurerm" {
-  features {
-    resource_provider {
-      register_defaults = false
-    }
-  }
+  features {}
+  skip_provider_registration = true
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
-
 
 # Variables
 variable "subscription_id" {

9c1495022201be4c565641946f795a632d97dc01
farhanhameed010 <farhanhameed010@gmail.com>
2024-12-30 12:02:51 +0500

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,177 @@
+# File: terraform/main.tf
+provider "azurerm" {
+  features {}
+  subscription_id                   = var.subscription_id
+  client_id                        = var.client_id
+  client_secret                    = var.client_secret
+  tenant_id                        = var.tenant_id
+  resource_provider_registrations   = "none"
+}
+
+# Variables
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+  default     = "1-e333aa8e-playground-sandbox"
+}
+
+# Data Source for Existing Resource Group
+data "azurerm_resource_group" "existing_rg" {
+  name = var.resource_group_name
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Public Subnet
+resource "azurerm_subnet" "public_subnet" {
+  name                 = "myPublicSubnet"
+  resource_group_name  = data.azurerm_resource_group.existing_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Private Subnet
+resource "azurerm_subnet" "private_subnet" {
+  name                 = "myPrivateSubnet"
+  resource_group_name  = data.azurerm_resource_group.existing_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "myPublicIP"
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  allocation_method   = "Static"
+  sku                = "Standard"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myNSG"
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# NSG Association
+resource "azurerm_subnet_network_security_group_association" "public_subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.public_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "myNIC"
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+
+  ip_configuration {
+    name                          = "myNICConfiguration"
+    subnet_id                     = azurerm_subnet.public_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "myVM"
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Outputs
+output "resource_group_name" {
+  value = data.azurerm_resource_group.existing_rg.name
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
+
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.vm.name
+}
+
---
+++ terraform/terrafiorm.tfvars
@@ -0,0 +1,6 @@
+# File: terraform/terraform.tfvars
+subscription_id      = env("TF_VAR_subscription_id")
+client_id           = env("TF_VAR_client_id")
+client_secret       = env("TF_VAR_client_secret")
+tenant_id           = env("TF_VAR_tenant_id")
+resource_group_name = env("TF_VAR_resource_group_name")
